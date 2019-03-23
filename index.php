ff870475dca1ab56445a812d992337ebe5a743dd
batistaTiago <ekyidag@gmail.com>
2019-03-23 15:44:13 -0300

    substituido idade por data de nascimento

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <a href="cadastro.php">Cadastro</a>
     <a href="login.php">Login</a>
 
-    	<span><span class="text-danger display-1 font-weight-bold">TODO</span>: REDIRECIONAR PARA HOME OU LOGIN DEPENDENDO DA SESSION. </span>
+    	<?php 
+    	echo '<span><span class="text-danger display-1 font-weight-bold">TODO</span>: REDIRECIONAR PARA HOME OU LOGIN DEPENDENDO DA SESSION. </span>' 
+    	?>
     	
 </body>
 </html>
